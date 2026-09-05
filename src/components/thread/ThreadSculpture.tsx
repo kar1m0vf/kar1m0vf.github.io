9d86243a49a20ae1d6d7daed1141d98c34d574be
kar1m0vf <kamilkarimov16092006@gmail.com>
2026-09-05 21:38:19 +0400

    fix: allow optional hero ready callback

--- src/components/thread/ThreadSculpture.tsx
+++ src/components/thread/ThreadSculpture.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import { motion, useTransform, type MotionValue } from 'motion/react';
 
 interface ThreadSculptureProps {
-  onReady?: () => void;
+  onReady?: (() => void) | undefined;
   progress: MotionValue<number>;
   reduced: boolean;
 }
